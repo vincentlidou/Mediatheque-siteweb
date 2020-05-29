5faa2495b7ad89a81820d2ede1097c85399f0c57
vincentlidou <56677829+vincentlidou@users.noreply.github.com>
2020-05-29 15:53:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+include_once ('includes.php');
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="fr"><meta charset="UTF-8">
+<head>
+<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>GSB</title>
+</head>
+<body>
+
+	<h1 class="index-h1">Outil de Consultation GSB</h1>
+
+<div class="acc_ctn">
+	<p> 
+		Bienvenue sur GSB Web, consultez la liste des médecins répertoriés avec cet outil.
+	</p>
+
+	<div class="acc_chx">
+			<a id="consulter"  href="./Connexion.php">Connexion</a>
+			<a id="inscription" href="./inscription.php">Inscription</a>
+	</div>
+</div>
+	
+</body>
+</html>
